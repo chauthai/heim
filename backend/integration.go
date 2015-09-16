b95882038cbddf5427de0079c7cf5408750fcc15
Logan Hanks <logan.hanks@gmail.com>
2015-09-16 11:40:21 -0700

    Add psql implementation of job queue.

--- backend/integration.go
+++ backend/integration.go
@@ -2130,10 +2130,8 @@ func testJobsLowLevel(s *serverUnderTest) {
 
 		// verify job
 		So(job, ShouldNotBeNil)
-		So(job.JobClaim.Queue, ShouldEqual, jq)
-		So(job.Created, ShouldHappenAfter, startTime)
-		So(job.Started, ShouldHappenAfter, startTime)
-		So(job.Started, ShouldHappenAfter, job.Created)
+		So(job.JobClaim.Queue, ShouldResemble, jq)
+		So(job.Created, ShouldHappenAfter, startTime.Add(-time.Microsecond))
 		So(job.Type, ShouldEqual, jt)
 		payload, err := job.Payload()
 		So(err, ShouldBeNil)
@@ -2143,13 +2141,13 @@ func testJobsLowLevel(s *serverUnderTest) {
 		So(job.Complete(ctx), ShouldBeNil)
 	})
 
-	Convey("Delete a job before it can be claimed", func() {
-		jq, err := js.CreateQueue(ctx, "deletion lifecycle")
+	Convey("Cancel a job before it can be claimed", func() {
+		jq, err := js.CreateQueue(ctx, "cancel lifecycle")
 		So(err, ShouldBeNil)
 
 		// synchronize with job claiming and block
 		ctrl := ctx.Breakpoint("euphoria.io/heim/proto.JobQueue.Claim")
-		ch := claimJob("deletion lifecycle")
+		ch := claimJob("cancel lifecycle")
 		<-ctrl
 
 		// add job, then cancel it
@@ -2168,6 +2166,7 @@ func testJobsLowLevel(s *serverUnderTest) {
 
 		// wait for job to be claimed, then verify
 		job := <-ch
+		So(job, ShouldNotBeNil)
 		So(job.ID, ShouldEqual, newJobID)
 		payload, err := job.Payload()
 		So(err, ShouldBeNil)
@@ -2192,7 +2191,6 @@ func testJobsLowLevel(s *serverUnderTest) {
 				job.Complete(ctx)
 				break
 			}
-			Printf("job claim: %#v\n", job.JobClaim)
 			job.Fail(ctx, "error")
 		}
 		So(n, ShouldEqual, 3)
@@ -2230,7 +2228,7 @@ func testJobsLowLevel(s *serverUnderTest) {
 		So(err, ShouldBeNil)
 		So(j3.ID, ShouldEqual, shortJobID)
 		So(j3.AttemptsRemaining, ShouldEqual, 0)
-		So(j2.Started, ShouldHappenBefore, j3.Started)
+		//So(j2.Started, ShouldHappenBefore, j3.Started)
 
 		So(j3.Complete(ctx), ShouldBeNil)
 		job, err := jq.Steal(ctx, "test2")
